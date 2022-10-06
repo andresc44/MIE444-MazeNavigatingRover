895c09bf874b37ddbd2cd56da9bf297630cd4623
Andres Cervera <andres.cervera.rozo@gmail.com>
2022-10-06 16:08:54 -0400

    Main file created, first local push

---
+++ Arduino/main/main.ino
@@ -0,0 +1,11 @@
+int pin = 1;
+//  Congrats, you can see this message!
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
