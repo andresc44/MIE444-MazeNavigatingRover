bf5609ceb5d6f8caad87ffa345808b18f97580d5
Andres Cervera Rozo <andres.cervera.rozo@gmail.com>
2022-10-17 22:44:22 -0400

    Actual cloning of packages as submodules

--- ROS/scripts/src/ArduinoCommands.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env python3
-# Where all of the motor, lights, buzzer, and servo messages will be published
--- ROS/scripts/src/Bluetooth.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env python3
-# where Pi will communicate over bluetooth to whoever is listening
