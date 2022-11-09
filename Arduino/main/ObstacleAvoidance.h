f398d3e82b1b95c5ff2a38f18caf500d68155f87
Christopher Tong <christong@Christophers-MacBook-Pro-2.local>
2022-11-09 13:16:23 -0500

    Chris node update

--- Arduino/main/ObstacleAvoidance.h
+++ Arduino/main/ObstacleAvoidance.h
@@ -3,7 +3,7 @@ float pingTime1 = 0;
 float pingTime2 = 0;
 float pingTime3 = 0;
 float pingTime4 = 0;
-float pingTime5 = 0;
+//float pingTime5 = 0; //for sweeping mechanisms
 
 struct UltraArray {
     uint16_t array[5];
@@ -30,7 +30,7 @@ void Ultrasonic_Heartbeat() {
   digitalWrite(TRIGPIN2, LOW);
   digitalWrite(TRIGPIN3, LOW);
   digitalWrite(TRIGPIN4, LOW);
-  digitalWrite(TRIGPIN5, LOW);
+  //digitalWrite(TRIGPIN5, LOW);
   
   delayMicroseconds(2000);
   
@@ -38,7 +38,7 @@ void Ultrasonic_Heartbeat() {
   digitalWrite(TRIGPIN2, HIGH);
   digitalWrite(TRIGPIN3, HIGH);
   digitalWrite(TRIGPIN4, HIGH);
-  digitalWrite(TRIGPIN5, HIGH);
+  //digitalWrite(TRIGPIN5, HIGH);
   
   delayMicroseconds(10);
   
@@ -46,17 +46,20 @@ void Ultrasonic_Heartbeat() {
   digitalWrite(TRIGPIN2, LOW);
   digitalWrite(TRIGPIN3, LOW);
   digitalWrite(TRIGPIN4, LOW);
-  digitalWrite(TRIGPIN5, LOW);
+  //digitalWrite(TRIGPIN5, LOW);
   
   pingTime1 = pulseIn (ECHOPIN1, HIGH); //unit us
   pingTime2 = pulseIn (ECHOPIN2, HIGH); //unit us
   pingTime3 = pulseIn (ECHOPIN3, HIGH); //unit us
   pingTime4 = pulseIn (ECHOPIN4, HIGH); //unit us
-  pingTime5 = pulseIn (ECHOPIN5, HIGH); //unit us
+  //pingTime5 = pulseIn (ECHOPIN5, HIGH); //unit us
 
   ultra.array[0] = pingTime1 * SPEEDSOUND; //unit mm
   ultra.array[1] = pingTime2 * SPEEDSOUND; //unit mm
   ultra.array[2] = pingTime3 * SPEEDSOUND; //unit mm
   ultra.array[3] = pingTime4 * SPEEDSOUND; //unit mm
-  ultra.array[4] = pingTime5 * SPEEDSOUND; //unit mm
+  //ultra.array[4] = pingTime5 * SPEEDSOUND; //unit mm
+
+  // publish array of 4
+  // insert rosserial code here? or in the main?
 }
