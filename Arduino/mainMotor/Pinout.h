b7dc4442b217150d7729e8e44e8ec81a74c15a19
Liam Toner <liamtoner@Liams-MacBook-Pro.local>
2022-11-09 14:49:48 -0500

    message

--- Arduino/mainMotor/Pinout.h
+++ Arduino/mainMotor/Pinout.h
@@ -1,43 +1,19 @@
-const uint8_t TRIGPIN1 = 9;  //digital output
-const uint8_t ECHOPIN1 = 10; //digital input
-const uint8_t TRIGPIN2 = 9;
-const uint8_t ECHOPIN2 = 10;
-const uint8_t TRIGPIN3 = 9;
-const uint8_t ECHOPIN3 = 10;
-const uint8_t TRIGPIN4 = 9;
-const uint8_t ECHOPIN4 = 10;
-const uint8_t TRIGPIN5 = 9;
-const uint8_t ECHOPIN5 = 10;
+const uint8_t ENCODER1A = 3; //digital input
+const uint8_t ENCODER1B = 6;
+const uint8_t ENCODER2A = 4;
+const uint8_t ENCODER2B = 7;
+const uint8_t ENCODER3A = 2;
+const uint8_t ENCODER3B = 5;
 
-const uint8_t IRPIN1   = 10; //digital input
-const uint8_t IRPIN2   = 10;
-const uint8_t IRPIN3   = 10;
-const uint8_t IRPIN4   = 10;
-
-const uint8_t DIPPIN1  = 10; //digital input
-const uint8_t DIPPIN2  = 10;
-
-const uint8_t LIGHTPIN = 10; //digital output (possibly pwm)
-const uint8_t SOUNDPIN = 10; //digital output (possibly pwm)
-
-const uint8_t SERVOPIN = 10; //analog output (pwm)
-
-const uint8_t ENCODER1A = 10; //digital input
-const uint8_t ENCODER1B = 10;
-const uint8_t ENCODER2A = 10;
-const uint8_t ENCODER2B = 10;
-const uint8_t ENCODER3A = 10;
-const uint8_t ENCODER3B = 10;
-
-const uint8_t DIR1A = 10; //digital output
-const uint8_t DIR1B = 10;
-const uint8_t DIR2A = 10;
-const uint8_t DIR2B = 10;
-const uint8_t DIR3A = 10;
-const uint8_t DIR3B = 10;
+const uint8_t DIR1A = A2; //digital output
+const uint8_t DIR1B = A3;
+const uint8_t DIR2A = A4;
+const uint8_t DIR2B = A5;
+const uint8_t DIR3A = A0;
+const uint8_t DIR3B = A1;
 
 const uint8_t EN1 = 10; //analog output (PWM)
-const uint8_t EN2 = 10;
-const uint8_t EN3 = 10;
+const uint8_t EN2 = 11;
+const uint8_t EN3 = 9;
 
 //TOF Sensor must be connected to SDA and SCL pins (address of 0x29)
