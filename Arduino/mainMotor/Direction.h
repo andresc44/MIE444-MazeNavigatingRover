41a35529ece22ae55a27a7e9d57542ea355b2e7a
Andres Cervera Rozo <andres.cervera.rozo@gmail.com>
2022-11-12 14:25:12 -0500

    State of project with first successful full communication

--- Arduino/mainMotor/Direction.h
+++ Arduino/mainMotor/Direction.h
@@ -1,6 +1,5 @@
 //direction the robot will go in relative to car body
-#include <ros.h>
-#include <std_msgs/Int8MultiArray.h> // pwm data is sent as 8 bit array
+#include <std_msgs/Int16MultiArray.h> // pwm data is sent as 16 bit array
 
 //Creating a Callback Function for Subscriber
 //Input: standard message
@@ -9,7 +8,43 @@
 //uint8_t pwm_array[3] = {0, 0, 0}; //initialize pwm array
 //bool dir_CW_array[3] = {HIGH, HIGH, HIGH}; //initialize direction array
 
-ros::Subscriber<std_msgs::Int8MultiArray> sub("wheelsPWM", &messageCb );
+
+
+// Control feedback code
+void Ajust_PWM (int16_t pwm[]) {
+  // LIAM Put in all the PID code here
+  //can also accept rpms if preferred
+  analogWrite(EN1, pwm[0]);
+  analogWrite(EN2, pwm[1]);
+  analogWrite(EN3, pwm[2]);
+}
+
+void messageCb(const std_msgs::Int16MultiArray& pwm_msg) {
+  //every time call back gets called it listens to topic for new pwm_msg and updates the motor values
+  //pwm_msg will be an integer between -255 and 255
+  // negative int is counter clockwise, positive int is clockwise
+  //when you index into something you have to use .data extension
+  //pwm_array = {abs(pwm_msg.data[0]),abs(pwm_msg.data[1]),abs(pwm_msg.data[2])}; // stores pwm values for motor 1,2,3
+  //dir_CW_array = {pwm_msg.data[0] >= 0, pwm_msg.data[1] >= 0, pwm_msg.data[2] >= 0};
+  //pwm_msg.data = {sign1, pwm1, sign2, pwm2, sign3, pwm3};
+   
+ //input 1 is HIGH-> clockwise
+ //input 2 is LOW -> clockwise
+  digitalWrite(DIR1A, pwm_msg.data[0]);
+  digitalWrite(DIR1B, !pwm_msg.data[0]);
+  digitalWrite(DIR2A, pwm_msg.data[2]);
+  digitalWrite(DIR2B, !pwm_msg.data[2]);
+  digitalWrite(DIR3A, pwm_msg.data[4]);
+  digitalWrite(DIR3B, !pwm_msg.data[4]);
+
+  analogWrite(EN1, pwm_msg.data[1]);
+  analogWrite(EN2, pwm_msg.data[3]);
+  analogWrite(EN3, pwm_msg.data[5]);
+  //Adjust_PWM(pwm_msg); //Where Liam's code will go
+}
+
+
+ros::Subscriber<std_msgs::Int16MultiArray> sub("wheelsPWM", &messageCb );
 
 void DC_Motor_Setup() {
   nh.subscribe(sub); //subscribe node nh to topic wheelsPWM
@@ -40,38 +75,4 @@ void DC_Motor_Setup() {
   digitalWrite(DIR3A, LOW);
   digitalWrite(DIR3B, LOW);
   Serial.println(F("Motors Ready"));
-}
-
-
-// Control feedback code
-void Ajust_PWM (uint8_t pwm[]) {
-  // LIAM Put in all the PID code here
-  //can also accept rpms if preferred
-  analogWrite(EN1, pwm[0]);
-  analogWrite(EN2, pwm[1]);
-  analogWrite(EN3, pwm[2]);
-}
-
-void messageCb(const std_msgs::Int8MultiArray& pwm_msg) {
-  //every time call back gets called it listens to topic for new pwm_msg and updates the motor values
-  //pwm_msg will be an integer between -255 and 255
-  // negative int is counter clockwise, positive int is clockwise
-  //when you index into something you have to use .data extension
-  //pwm_array = {abs(pwm_msg.data[0]),abs(pwm_msg.data[1]),abs(pwm_msg.data[2])}; // stores pwm values for motor 1,2,3
-  //dir_CW_array = {pwm_msg.data[0] >= 0, pwm_msg.data[1] >= 0, pwm_msg.data[2] >= 0};
-  //pwm_msg.data = {sign1, pwm1, sign2, pwm2, sign3, pwm3};
-   
- //input 1 is HIGH-> clockwise
- //input 2 is LOW -> clockwise
-  digitalWrite(DIR1A, pwm_msg.data[0]);
-  digitalWrite(DIR1B, !pwm_msg.data[0]);
-  digitalWrite(DIR2A, pwm_msg.data[2]);
-  digitalWrite(DIR2B, !pwm_msg.data[2]);
-  digitalWrite(DIR3A, pwm_msg.data[3]);
-  digitalWrite(DIR3B, !pwm_msg.data[3]);
-
-  analogWrite(EN1, pwm_msg.data[1]);
-  analogWrite(EN2, pwm_msg.data[2]);
-  analogWrite(EN3, pwm_msg.data[3]);
-  //Adjust_PWM(pwm_msg); //Where Liam's code will go
 }
--- Arduino/mainMotor/mainMotor.ino
+++ Arduino/mainMotor/mainMotor.ino
@@ -2,11 +2,12 @@
 //Authors: Andres Cervera Rozo, Christopher Tong,
 //         Liam Toner, Nathalie Cristofaro
 //good reference link https://github.com/ros-drivers/rosserial/blob/noetic-devel/rosserial_arduino/src/ros_lib/tests/float64_test/float64_test.pde
-
+#include <ros.h>
+ros::NodeHandle nh; //initialize node handle object
 //#include "Accessories.h"
 //#include "BlockRetrieval.h"
-#include "Direction.h"
 #include "Pinout.h" // liam will modify pinout header to reflect electrnoics
+#include "Direction.h"
 
 
 //Constants as CONSTANT
@@ -15,10 +16,10 @@
 //Class as Class
 
 uint16_t tof = 0; 
-ros::NodeHandle nh; //initialize node handle object
 
 void setup() {
   nh.initNode(); // initialize the Ros node handle
+//  nh.getHardware()->setBaud(115200);
   Serial.begin(9600);
   DC_Motor_Setup();
   //Accessory_Setup();
