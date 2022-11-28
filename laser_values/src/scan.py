56a6ac94a5b2e42b21525cae7d4270ad39dd3f55
Andres Cervera Rozo <andres.cervera.rozo@gmail.com>
2022-11-27 20:53:37 -0500

    get all 4 distances

--- laser_values/src/scan.py
+++ laser_values/src/scan.py
@@ -4,7 +4,14 @@ import rospy
 from sensor_msgs.msg import LaserScan
 
 def callback(msg):
-    print (msg.ranges[719])
+    print('\n\n Straight: ')
+    print (msg.ranges[0])
+    print('\t Left: ')
+    print (msg.ranges[180])
+    print('\t Backward: ')
+    print (msg.ranges[360])
+    print('\t Right: ')
+    print (msg.ranges[540])
     # print(len(msg.ranges))
 
 rospy.init_node('scan_values') 
